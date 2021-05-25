298f8af68407ced62ab20c44212fe0707cead8ae
HaiTran6692 <62434142+HaiTran6692@users.noreply.github.com>
2021-05-25 17:10:40 +0200

    v2a.

--- PrijemkaHostivice/PrijemkaHostivice/FormMain.Designer.cs
+++ PrijemkaHostivice/PrijemkaHostivice/FormMain.Designer.cs
@@ -43,11 +43,12 @@ namespace PrijemkaHostivice
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.placeHolderTextBox2 = new PrijemkaHostivice.PlaceHolderTextBox();
-            this.placeHolderTextBox1 = new PrijemkaHostivice.PlaceHolderTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.placeHolderTextBox1 = new PrijemkaHostivice.PlaceHolderTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -152,7 +153,7 @@ namespace PrijemkaHostivice
             // pictureBox3
             // 
             this.pictureBox3.Image = global::PrijemkaHostivice.Properties.Resources.Apps_Dialog_Apply_icon;
-            this.pictureBox3.Location = new System.Drawing.Point(1026, 6);
+            this.pictureBox3.Location = new System.Drawing.Point(1005, 6);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(48, 48);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -164,7 +165,7 @@ namespace PrijemkaHostivice
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(1442, 37);
+            this.textBox1.Location = new System.Drawing.Point(1418, 38);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(109, 13);
@@ -177,6 +178,7 @@ namespace PrijemkaHostivice
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.placeHolderTextBox2);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
@@ -193,34 +195,46 @@ namespace PrijemkaHostivice
             this.panel1.Size = new System.Drawing.Size(1897, 69);
             this.panel1.TabIndex = 7;
             // 
+            // placeHolderTextBox2
+            // 
+            this.placeHolderTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.placeHolderTextBox2.ForeColor = System.Drawing.Color.Gray;
+            this.placeHolderTextBox2.Location = new System.Drawing.Point(804, 34);
+            this.placeHolderTextBox2.Name = "placeHolderTextBox2";
+            this.placeHolderTextBox2.PlaceHolderText = "Číslo příjemky...";
+            this.placeHolderTextBox2.Size = new System.Drawing.Size(195, 20);
+            this.placeHolderTextBox2.TabIndex = 8;
+            this.placeHolderTextBox2.Text = "Číslo příjemky...";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1562, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Příjemka č.";
+            // 
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1359, 37);
+            this.label4.Location = new System.Drawing.Point(1340, 38);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Objednávka č.";
             // 
-            // label3
+            // textBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2, 81);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Double click to see detail ↓";
-            // 
-            // placeHolderTextBox2
-            // 
-            this.placeHolderTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
-            this.placeHolderTextBox2.ForeColor = System.Drawing.Color.Gray;
-            this.placeHolderTextBox2.Location = new System.Drawing.Point(842, 34);
-            this.placeHolderTextBox2.Name = "placeHolderTextBox2";
-            this.placeHolderTextBox2.PlaceHolderText = null;
-            this.placeHolderTextBox2.Size = new System.Drawing.Size(157, 20);
-            this.placeHolderTextBox2.TabIndex = 8;
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(1630, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(129, 13);
+            this.textBox2.TabIndex = 6;
             // 
             // placeHolderTextBox1
             // 
@@ -234,15 +248,14 @@ namespace PrijemkaHostivice
             this.placeHolderTextBox1.Text = "Nhập số đơn hàng / Zadejte číslo objednávky...";
             this.placeHolderTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.placeHolderTextBox1_KeyDown);
             // 
-            // textBox2
+            // label3
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(1557, 37);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(129, 13);
-            this.textBox2.TabIndex = 6;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Double click to see detail ↓";
             // 
             // FormMain
             // 
@@ -256,7 +269,7 @@ namespace PrijemkaHostivice
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PrijemkaHostivice v1b.21,01.2021";
+            this.Text = "PrijemkaHostivice v2a.25.05.2021";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -288,6 +301,7 @@ namespace PrijemkaHostivice
         private System.Windows.Forms.Label label4;
         private PlaceHolderTextBox placeHolderTextBox2;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- PrijemkaHostivice/PrijemkaHostivice/FormMain.cs
+++ PrijemkaHostivice/PrijemkaHostivice/FormMain.cs
@@ -20,7 +20,7 @@ namespace PrijemkaHostivice
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.WindowState= FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Maximized;
             dateTimePicker1from.Format = DateTimePickerFormat.Custom;
             dateTimePicker1from.CustomFormat = "dd.MM.yyyy";
             dateTimePicker1from.Value = DateTime.Today;
@@ -28,7 +28,7 @@ namespace PrijemkaHostivice
             dateTimePicker2to.Format = DateTimePickerFormat.Custom;
             dateTimePicker2to.CustomFormat = "dd.MM.yyyy";
             dateTimePicker2to.Value = DateTime.Today;
-            
+
             LoadPrijemky();
         }
         List<InvoiceDetail> _List = new List<InvoiceDetail>();
@@ -46,7 +46,7 @@ namespace PrijemkaHostivice
         private void TimTheoSoDon(string inputOBJ, string inputPrijemka)
         {
             crystalReportViewer1.ReportSource = null;
-            _List.Clear();     
+            _List.Clear();
             string sqlERP1 = $@"select 
                             format(p.created,'dd.MM.yyyy') as datum_prijmu --0
                             ,f.dodavatel--1
@@ -75,8 +75,8 @@ namespace PrijemkaHostivice
                         {
                             kod_zbozi = TB_erp.Rows[i][3].ToString(),
                             nazev = TB_erp.Rows[i][4].ToString(),
-                            objednano = TB_erp.Rows[i][5].ToString(),
-                            prjato = TB_erp.Rows[i][6].ToString()
+                            prjato = TB_erp.Rows[i][5].ToString(),
+                            objednano = TB_erp.Rows[i][6].ToString() //dph
                         });
                     }
                     int cisloDodavatele = int.Parse(TB_erp.Rows[0][1].ToString().Substring(1, TB_erp.Rows[0][1].ToString().Length - 1));
@@ -88,18 +88,18 @@ namespace PrijemkaHostivice
                     if (TB.Rows.Count >= 1)
                     {
                         CrystalReport2 cr12 = new CrystalReport2();
-                        cr12.SetDataSource(_List);                       
+                        cr12.SetDataSource(_List);
                         string cislodod = "D" + TB.Rows[0][0].ToString();
                         string nazev = TB.Rows[0][1].ToString();
                         string ico = "IČ: " + TB.Rows[0][2].ToString();
                         string dic = "DIČ: " + TB.Rows[0][3].ToString();
                         string street = TB.Rows[0][4].ToString();
                         string city = TB.Rows[0][6].ToString() + " " + TB.Rows[0][5].ToString();
-                        string obj = "Objednávka č."+inputOBJ;
-                        string prijemka = "PŘÍJEMKA: "+inputOBJ;
+                        string obj = "Objednávka č." + inputOBJ;
+                        string prijemka = "PŘÍJEMKA: " + inputPrijemka;
                         string datumPrijmuD = TB_erp.Rows[0][0].ToString();
                         Zen.Barcode.Code128BarcodeDraw bar1 = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
-                        bar1.Draw(inputOBJ,40).Save(Application.StartupPath + "\\cache\\file_EAN_obj.png");
+                        bar1.Draw(inputOBJ, 40).Save(Application.StartupPath + "\\cache\\file_EAN_obj.png");
 
                         cr12.SetParameterValue("EAN", Application.StartupPath + "\\cache\\file_EAN_obj.png"); //location  
                         cr12.SetParameterValue("prijemka", prijemka);
@@ -112,17 +112,17 @@ namespace PrijemkaHostivice
                         cr12.SetParameterValue("dicD", dic);
                         cr12.SetParameterValue("dicD", dic);
                         cr12.SetParameterValue("datumPrijmuD", datumPrijmuD);
-                       
+
                         crystalReportViewer1.ReportSource = cr12;
                     }
                 }
             }
-            catch (Exception )
+            catch (Exception)
             {
-                MessageBox.Show("Xảy ra lỗi") ;
+                MessageBox.Show("Xảy ra lỗi");
             }
 
-        }    
+        }
         private void pictureBox4_Click(object sender, EventArgs e)
         {
             crystalReportViewer1.PrintReport();
@@ -131,8 +131,7 @@ namespace PrijemkaHostivice
         {
             string sqlLoadPrijemky = $@"SELECT      
                                       f.cislo as Cislo_prijemky
-                                      ,f.cisloobj as Cislo_objednavky      
-                                      ,f.[Casti]      
+                                      ,f.cisloobj as Cislo_objednavky --  ,f.[Casti]      
                                       ,FORMAT(ISNULL(f.datum_prijemky,f.[datakt]),'dd.MM.yyyy HH:mm') as Datum_cas_prijemky 
                                       ,d.Nazev
                                       ,IIF([dodavatelske_cislo] like 'DL%',[dodavatelske_cislo],'') as Cislo_dodaciho_listu	  
@@ -141,7 +140,7 @@ namespace PrijemkaHostivice
                                   FROM [OrdersManager].[dbo].[Faktury] f
                                   LEFT JOIN [TDF Database].dbo.dodavatele d  ON d.cislodod = f.dodavatel
                                   WHERE  datediff(day,convert(datetime,'{dateTimePicker1from.Value.ToString("dd.MM.yyyy")}',104),ISNULL(f.datum_prijemky,f.[datakt]))>=0
-                                  ORDER by f.cislo,f.[Casti]  ";
+                                  ORDER by f.cislo ,f.cisloobj  ";
             DataTable TB = new DataTable();
             try
             {
@@ -151,22 +150,29 @@ namespace PrijemkaHostivice
                     dataGridView1.DataSource = TB;
                     dataGridView1.Columns[0].HeaderText = "Číslo příjemky";
                     dataGridView1.Columns[1].HeaderText = "Číslo objednávky";
-                    dataGridView1.Columns[2].HeaderText = "Části";
-                    dataGridView1.Columns[3].HeaderText = "Datum příjmu";
-                    dataGridView1.Columns[4].HeaderText = "Název";
-                    dataGridView1.Columns[5].HeaderText = "Dodací list";
-                    dataGridView1.Columns[6].HeaderText = "Faktura";
-                    dataGridView1.Columns[7].HeaderText = "Index";
+                    dataGridView1.Columns[2].HeaderText = "Datum příjmu";
+                    dataGridView1.Columns[3].HeaderText = "Název";
+                    dataGridView1.Columns[4].HeaderText = "Dodací list";
+                    dataGridView1.Columns[5].HeaderText = "Faktura";
+                    dataGridView1.Columns[6].HeaderText = "Index";
+
+                    for (int i = 0; i < dataGridView1.Columns.Count - 1; i++)
+                    {
+                        dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+                    }
+                    dataGridView1.Columns[dataGridView1.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
                 }
                 else
                 {
                     MessageBox.Show("Không có đơn hàng nào!");
                 }
             }
-            catch (Exception )
+            catch (Exception ex)
             {
-                MessageBox.Show(sqlLoadPrijemky);
-            }        
+                MessageBox.Show(ex.ToString());
+            }
 
         }
         private void pictureBox1_Click_1(object sender, EventArgs e)
@@ -183,10 +189,10 @@ namespace PrijemkaHostivice
         {
             try
             {
-                DataGridViewRow row = new DataGridViewRow();               
+                DataGridViewRow row = new DataGridViewRow();
                 row = dataGridView1.Rows[e.RowIndex];
-                textBox2.Text= row.Cells[0].Value.ToString();
-                textBox1.Text = row.Cells[1].Value.ToString();               
+                textBox2.Text = row.Cells[0].Value.ToString();
+                textBox1.Text = row.Cells[1].Value.ToString();
             }
             catch (Exception)
             {
