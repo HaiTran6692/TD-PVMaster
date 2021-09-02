05c4899fcf6f58c44061698f775a3a7008e1569f
HaiTran6692 <62434142+HaiTran6692@users.noreply.github.com>
2021-09-02 11:22:19 +0200

    w3

--- PVMaster/PVMaster/BranchInfor.cs
+++ PVMaster/PVMaster/BranchInfor.cs
@@ -12,7 +12,7 @@ namespace PVMaster
         public static string TDF_street = "Lisbušská 319/126";
         public static string TDF_city = "142 00 Praha 411-Libuš";
         public static string TDF_ucet = "Číslo účtu: 123-20780227/0100 Komerční banka a.s.";
-        public static string TDF_tel = "+420 774 458 383";
+        public static string TDF_tel = "Tel: +420 774 458 383";
         public static string TDF_ico = "IČ: 27160840";
         public static string TDF_dic = "DIČ: CZ27160840";
 
@@ -20,16 +20,16 @@ namespace PVMaster
         public static string TDB_street = "Olomoucká 1193/61a";
         public static string TDB_city = "62700 Brno-Černovice ";
         public static string TDB_ucet = "Číslo účtu: 123-2736930277/0100 Komerční banka a.s.";
-        public static string TDB_tel = "+420 777 958 383";
+        public static string TDB_tel = "Tel: +420 777 958 383";
         public static string TDB_ico = "IČ: 09617612";
         public static string TDB_dic = "DIČ: CZ09617612";
 
 
-        public static string TDU_name = "TAMDA ÚSTÍ     s.r.o";
+        public static string TDU_name = "TAMDA ÚSTÍ s.r.o";
         public static string TDU_street = "Havířská 352/17";
         public static string TDU_city = "40010 Ústí nad Labem-město";
         public static string TDU_ucet = "Číslo účtu: 123-2737920257/0100 Komerční banka a.s.";
-        public static string TDU_tel = "+420 773 321 123";
+        public static string TDU_tel = "Tel: +420 773 321 123";
         public static string TDU_ico = "IČ: 09617639";
         public static string TDU_dic = "DIČ: CZ09617639";
 
